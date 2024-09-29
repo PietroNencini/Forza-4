aed029192d872f3114629e221629375417666ee4
Pietro Nencini <nencinipietro2006@gmail.com>
2024-09-29 10:51:35 +0200

    Fixed syncronized color turn

--- javascript/script.js
+++ javascript/script.js
@@ -5,9 +5,6 @@ const COLS = 7;
 const COLOR_1 = "red";
 const COLOR_2 = "yellow"
 
-let red_img = "./images/red.png";
-let yellow_img = "./images/yellow.png";
-
 let turno_rosso = true;
 
 let game_table = document.getElementById("tabella_gioco");
@@ -18,6 +15,19 @@ let logical_table = [
     [0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0] ];
+
+function updateGameTurn() {
+    let turn_paragraph = document.getElementById("turn_updater");
+    if(turno_rosso) {
+        turn_paragraph.innerHTML = "TURNO DI <b> ROSSO </b>";
+        turn_paragraph.style.color = "red";
+    } else {
+        turn_paragraph.innerHTML = "TURNO DI <b> GIALLO </b>";
+        turn_paragraph.style.color = "yellow";
+    }
+    turno_rosso = !turno_rosso;
+}
+
 
 function piazza_pedina(column) {
     for(let i = 5; i >= 0; i--) {
@@ -30,9 +40,11 @@ function piazza_pedina(column) {
             let colonna_dove_piazzare = game_cols[column -1];
             
             //colonna_dove_piazzare.style.backgroundColor = turno_rosso ? COLOR_1 : COLOR_2;
-            colonna_dove_piazzare.innerHTML = "";
-            colonna_dove_piazzare.appendChild();
-            turno_rosso = !turno_rosso;
+            let pedina_colorata = colonna_dove_piazzare.getElementsByClassName('bi')[0];
+            
+            pedina_colorata.style.color = turno_rosso ? "yellow" : "red";
+            //pedina_colorata.classList.add(turno_rosso ? "bg-danger" : "bg-warning");
+            updateGameTurn();
             break;
         }
     }
